94b998ead65fa01377088a7568f2126c6400caf7
front-end-magic <kemiowoyele3@gmail.com>
2023-08-23 15:08:19 +0100

    first commit

---
+++ js/gallery-array.js
@@ -0,0 +1,338 @@
+var flashItems = [
+
+    {
+        "aboutImage": "CSS & HTML ice-cream shop",
+        "seeMore": "See More",
+        "image": "images/card1.jpg",
+        "link": "https://codepen.io/kemiowoyele1/pen/KKVVvbN"
+    },
+    {
+        "aboutImage": "CSS & HTML Christmas Card",
+        "seeMore": "See More",
+        "image": "images/card2.jpg",
+        "link": "https://codepen.io/kemiowoyele1/pen/QWqaLNz"
+    },
+    {
+        "aboutImage": "CSS & HTML Sallah Greeting",
+        "seeMore": "See More",
+        "image": "images/card3.jpg",
+        "link": "https://codepen.io/kemiowoyele1/pen/rNObBav"
+    },
+    {
+        "aboutImage": "Baby First Words Learning App",
+        "seeMore": "See More",
+        "image": "images/happy-balloons.png",
+        "link": "happy-balloons/index.html"
+    },
+    {
+        "aboutImage": "CSS & HTML Birthday Cake",
+        "seeMore": "See More",
+        "image": "images/card4.jpg",
+        "link": "https://codepen.io/kemiowoyele1/pen/KKVZvQY"
+    },
+    {
+        "aboutImage": "CSS & HTML Space Ship",
+        "seeMore": "See More",
+        "image": "images/card5.jpg",
+        "link": "https://codepen.io/kemiowoyele1/pen/KKWNdMo"
+    },
+    {
+        "aboutImage": "CSS & HTML crying girl",
+        "seeMore": "See More",
+        "image": "images/card6.jpg",
+        "link": "https://codepen.io/kemiowoyele1/pen/xxZxrmy"
+    },
+    // {
+    //     "aboutImage": "baby phone chat page (Mobile App)",
+    //     "seeMore": "Download App",
+    //     "image": "images/card7.jpg",
+    //     "link": "#"
+    // },
+    // {
+    //     "aboutImage": "baby phone, video call page ",
+    //     "seeMore": "Download App",
+    //     "image": "images/card8.jpg",
+    //     "link": "#"
+    // },
+    // {
+    //     "aboutImage": "baby piano, animal sounds page ",
+    //     "seeMore": "Download App",
+    //     "image": "images/card9.jpg",
+    //     "link": "#"
+    // },
+    // {
+    //     "aboutImage": "baby piano, quiz selection page ",
+    //     "seeMore": "Download App",
+    //     "image": "images/card10.jpg",
+    //     "link": "#"
+    // },
+    // {
+    //     "aboutImage": "baby piano, alphabet and numbers flash card page ",
+    //     "seeMore": "Download App",
+    //     "image": "images/card11.jpg",
+    //     "link": "#"
+    // },
+    {
+        "aboutImage": "my baby laptop, home page ",
+        "seeMore": "Download App",
+        "image": "images/card12.jpg",
+        "link": "#"
+    },
+    // {
+    //     "aboutImage": "my baby phone, fan page ",
+    //     "seeMore": "Download App",
+    //     "image": "images/card13.jpg",
+    //     "link": "#"
+    // },
+    // {
+    //     "aboutImage": "my baby piano, home page ",
+    //     "seeMore": "Download App",
+    //     "image": "images/card14.jpg",
+    //     "link": "#"
+    // },
+    {
+        "aboutImage": "my baby music phone, home page ",
+        "seeMore": "Download App",
+        "image": "images/card15.jpg",
+        "link": "#"
+    },
+    {
+        "aboutImage": "my baby music phone, call page ",
+        "seeMore": "Download App",
+        "image": "images/card16.jpg",
+        "link": "#"
+    },
+    {
+        "aboutImage": "my baby music phone, piano page ",
+        "seeMore": "Download App",
+        "image": "images/card17.jpg",
+        "link": "#"
+    },
+    {
+        "aboutImage": "my baby music phone, animal sounds page ",
+        "seeMore": "Download App",
+        "image": "images/card18.jpg",
+        "link": "#"
+    },
+    {
+        "aboutImage": "my baby music phone, airplane page ",
+        "seeMore": "Download App",
+        "image": "images/card19.jpg",
+        "link": "#"
+    },
+    {
+        "aboutImage": "my baby music phone, call page ",
+        "seeMore": "Download App",
+        "image": "images/card20.jpg",
+        "link": "#"
+    },
+    {
+        "aboutImage": "my baby music phone, singing penguin ",
+        "seeMore": "Download App",
+        "image": "images/card21.jpg",
+        "link": "#"
+    },
+    {
+        "aboutImage": "bouncing balls",
+        "seeMore": "view page",
+        "image": "images/bounce.png",
+        "link": "pages/bouncinghead.html"
+    },
+    {
+        "aboutImage": " Hue Sliding Button ",
+        "seeMore": "view page",
+        "image": "images/slide.png",
+        "link": "pages/button9.html"
+    },
+    {
+        "aboutImage": "divs with random colors",
+        "image": "images/divs.png",
+        "link": "pages/createDivs.html"
+    },
+    {
+        "aboutImage": "Cat with animated ears",
+        "seeMore": "view page",
+        "image": "images/css-cat.png",
+        "link": "pages/cute-cat.html"
+    },
+    {
+        "aboutImage": "music player with vue js",
+        "seeMore": "See More",
+        "image": "images/music-player.png",
+        "link": "https://music-player-with-vuejs.netlify.app/#/"
+    },
+    {
+        "aboutImage": "School Project",
+        "seeMore": "See More",
+        "image": "images/edu.jpg",
+        "link": "https://edupower6.netlify.app/"
+    },
+    {
+        "aboutImage": "covid Project",
+        "seeMore": "See More",
+        "image": "images/covid.png",
+        "link": "https://tiidelab-stop-covid.netlify.app/"
+    },
+    {
+        "aboutImage": "covid Project",
+        "seeMore": "See More",
+        "image": "images/covid.png",
+        "link": "https://covid-19-real-info.netlify.app/"
+    },
+    {
+        "aboutImage": "covid Project",
+        "seeMore": "See More",
+        "image": "images/covid.png",
+        "link": "https://covid-19-real-info.netlify.app/"
+    },
+    {
+        "aboutImage": "CSS Moving train",
+        "seeMore": "See More",
+        "image": "images/card27.jpg",
+        "link": "https://css-moving-train.netlify.app/"
+    },
+    {
+        "aboutImage": "Simple interest calculator",
+        "seeMore": "See More",
+        "image": "images/simple.png",
+        "link": "https://vuesimpleinterest.netlify.app/"
+    },
+    {
+        "aboutImage": "CSS Car",
+        "seeMore": "See More",
+        "image": "images/CSScar.png",
+        "link": "https://css-rolling-car.netlify.app/"
+    },
+    {
+        "aboutImage": "Vue JS Music Player",
+        "seeMore": "See More",
+        "image": "images/vue-music.png",
+        "link": "https://musicplayer1111.netlify.app/#/"
+    },
+    {
+        "aboutImage": "Compound Interest Calculator",
+        "seeMore": "See More",
+        "image": "images/compound.png",
+        "link": "https://vue-compound-interest123.netlify.app/"
+    },
+    {
+        "aboutImage": "Vue JS E-commerce Frontend",
+        "seeMore": "See More",
+        "image": "images/gele.JPG",
+        "link": "https://chrismartvue.netlify.app/"
+    },
+    
+    {
+        "aboutImage": "Church Website",
+        "seeMore": "See More",
+        "image": "images/church.png",
+        "link": "https://christ-resurrection-and-salvation.netlify.app/"
+    },
+    {
+        "aboutImage": "Multiplication Table App",
+        "seeMore": "See More",
+        "image": "images/shuttle.png",
+        "link": "https://2times-table.netlify.app/"
+    },
+    {
+        "aboutImage": "javaScript fireworks",
+        "seeMore": "See More",
+        "image": "images/fire.png",
+        "link": "fireworks/firew.html"
+    },
+    {
+        "aboutImage": "Bubble Popping Game",
+        "seeMore": "See More",
+        "image": "images/bubbles.png",
+        "link": "happy-bubbles/index.html"
+    },
+    {
+        "aboutImage": "Honey Hunting Game",
+        "seeMore": "See More",
+        "image": "images/honey.png",
+        "link": "finder/index.html"
+    },
+    {
+        "aboutImage": "javaScript Fireworks",
+        "seeMore": "See More",
+        "image": "images/fireworks.png",
+        "link": "fireworks/firew.html"
+    },
+    {
+        "aboutImage": "Local Storage Todo List app",
+        "seeMore": "See More",
+        "image": "images/ls.png",
+        "link": "local-storage/index.html"
+    },
+    {
+        "aboutImage": "HTML & CSS Funny Face",
+        "seeMore": "See More",
+        "image": "images/face.png",
+        "link": "pages/grace.html"
+    },
+    {
+        "aboutImage": "HTML & CSS Alien Face",
+        "seeMore": "See More",
+        "image": "images/alien.png",
+        "link": "pages/green-alian.html"
+    },
+    {
+        "aboutImage": "HTML & CSS Humpty Dumpties",
+        "seeMore": "See More",
+        "image": "images/humpty.png",
+        "link": "pages/hupty.html"
+    },
+
+    {
+        "aboutImage": "HTML & CSS and JS spinner",
+        "seeMore": "See More",
+        "image": "images/spin.png",
+        "link": "pages/newSpin.html"
+    },
+
+    {
+        "aboutImage": "scroll effect",
+        "seeMore": "See More",
+        "image": "images/scroll.png",
+        "link": "pages/scrolleffect2.html"
+    },
+    {
+        "aboutImage": "space ship demo for wrong quiz answer",
+        "seeMore": "See More",
+        "image": "images/space1.png",
+        "link": "pages/spaceship-fail.html"
+    },
+
+    {
+        "aboutImage": "space ship demo for correct quiz answer",
+        "seeMore": "See More",
+        "image": "images/space2.png",
+        "link": "pages/spaceship.html"
+    },
+    {
+        "aboutImage": "HTML & CSS and JS spinner",
+        "seeMore": "See More",
+        "image": "images/spin2.png",
+        "link": "pages/spin.html"
+    },
+
+    {
+        "aboutImage": "HTML & CSS theatre",
+        "seeMore": "See More",
+        "image": "images/theatre.png",
+        "link": "pages/theatre.html"
+    },
+    {
+        "aboutImage": "HTML & CSS receipt app",
+        "seeMore": "See More",
+        "image": "images/cart.png",
+        "link": "pages/sumcart.html"
+    },
+
+
+
+
+
+
+]
+// console.log(flashItems.length)
---
+++ js/theme.js
@@ -0,0 +1,33 @@
+const blue = document.querySelector(".blue");
+const themeStyle = document.querySelector("#theme-style");
+const green = document.querySelector(".green");
+const grey = document.querySelector(".grey");
+const orange = document.querySelector(".orange");
+const pink = document.querySelector(".pink");
+const purple = document.querySelector(".purple");
+const teal = document.querySelector(".teal");
+const yellow = document.querySelector(".yellow");
+blue.addEventListener('click',()=>{
+    themeStyle.href = 'css/blue.css'; 
+} )
+green.addEventListener('click',()=>{
+    themeStyle.href = 'css/green.css'; 
+} )
+grey.addEventListener('click',()=>{
+    themeStyle.href = 'css/grey.css'; 
+} )
+orange.addEventListener('click',()=>{
+    themeStyle.href = 'css/orange.css'; 
+} )
+pink.addEventListener('click',()=>{
+    themeStyle.href = 'css/pink.css'; 
+} )
+purple.addEventListener('click',()=>{
+    themeStyle.href = 'css/purple.css'; 
+} )
+teal.addEventListener('click',()=>{
+    themeStyle.href = 'css/teal.css'; 
+} )
+yellow.addEventListener('click',()=>{
+    themeStyle.href = 'css/yellow.css'; 
+} )
